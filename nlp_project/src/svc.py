5fe3105151fed958602f0dbcc178f21c598a58fa
Cthulhu <philippe.talb@gmail.com>
2022-04-05 21:36:39 -0400

    Update svc.py

--- nlp_project/src/svc.py
+++ nlp_project/src/svc.py
@@ -23,7 +23,7 @@ def run_linearsvc(learning_df, x, y):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
     
     satisfaction_clf = Pipeline([('tfidf', TfidfVectorizer()),
-                         ('clf', LinearSVC()),
+                         ('clf', LinearSVC()), #squared hinge loss by default 1/2||w||^2  pour transformer la fonction loss='hinge'
                          ])
     satisfaction_clf.fit(x_train, y_train)  
     
@@ -46,7 +46,7 @@ def run_poly_svc(learning_df, x, y):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
     
     satisfaction_clf = Pipeline([('tfidf', TfidfVectorizer()),
-                         ('clf', SVC(kernel='poly', degree=3, gamma=2)),
+                         ('clf', SVC(kernel='poly', degree=3, gamma=2)), #RBF Kernel #Basic hinge loss by default
                          ])
     satisfaction_clf.fit(x_train, y_train)  
     
